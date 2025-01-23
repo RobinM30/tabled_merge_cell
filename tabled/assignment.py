b8103d2cd419c81c88fa0fb69b1d9ece70d80dc6
Robin MICHARD <48763322+RobinM30@users.noreply.github.com>
2025-01-23 16:15:34 +0100

    Update assignment.py

--- tabled/assignment.py
+++ tabled/assignment.py
@@ -213,22 +213,12 @@ def merge_multiline_rows(detection_result: TableResult, table_cells: List[SpanTa
             print(f"R2_Cel = {r2_cells}")
 
             
-            if gap > gap_thresh:
+            if gap > gap_thresh or len(r2_cols - r1_cols) > 0 or len(r2_cols) / len(all_cols) > 0.9:
                 row.row_id = len(new_rows)
                 new_rows.append(row)
+                current_cells = []
                 continue
-    
-            if len(r2_cols - r1_cols) > 0:
-                row.row_id = len(new_rows)
-                new_rows.append(row)
-                continue
-
-            # Ensure r2 has mostly blank cells
-            if len(r2_cols) / len(all_cols) > .9:
-                row.row_id = len(new_rows)
-                new_rows.append(row)
-                continue
-            
+                
             r1_idx = prev_row.row_id
             r2_idx = row.row_id
             detection_result.rows[r1_idx].bbox = [
