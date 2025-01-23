0598198dc77ae3dbd68837dd64e960ce3f9e3847
Robin MICHARD <48763322+RobinM30@users.noreply.github.com>
2025-01-23 14:38:36 +0100

    Update assignment.py

--- tabled/assignment.py
+++ tabled/assignment.py
@@ -254,7 +254,7 @@ def assign_rows_columns(detection_result: TableResult, image_size: list, heurist
     table_cells = initial_assignment(detection_result)
     merge_multiline_rows(detection_result, table_cells)
     table_cells = initial_assignment(detection_result,tresh = 0)
-    assign_overlappers(table_cells, detection_result = tresh = 0)
+    assign_overlappers(table_cells, detection_result, tresh = 0)
     total_unassigned = len([tc for tc in table_cells if tc.row_ids[0] is None or tc.col_ids[0] is None])
     print(f"Non assigné {total_unassigned}. \n"
     unassigned_frac = total_unassigned / max(len(table_cells), 1)
